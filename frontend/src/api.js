ed7831e723ac470eae3748d378c9329a1436ba25
Adnan Kazi <adnankazi0707@gmail.com>
2025-06-16 19:49:27 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-teacher-appointment-booking-3c00.onrender.com",
 });
 
 export default API;
