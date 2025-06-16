ec6fe4349e486f5ec08ab5f719814a0f6d5b1580
Adnan Kazi <adnankazi0707@gmail.com>
2025-06-16 19:06:20 +0530

    Backend code Added

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
+const {
+  getPendingStudents,
+  approveStudent,
+  addTeacher,
+  getTeachers,
+  updateTeacher,
+  deleteTeacher,
+} = require("../controllers/adminController");
+
+const router = express.Router();
+
+router.get("/students", protect, adminOnly, getPendingStudents);
+router.put("/students/:id/approve", protect, adminOnly, approveStudent);
+
+router.post("/teachers", protect, adminOnly, addTeacher);
+router.get("/teachers", protect, adminOnly, getTeachers);
+router.put("/teachers/:id", protect, adminOnly, updateTeacher);
+router.delete("/teachers/:id", protect, adminOnly, deleteTeacher);
+
+module.exports = router;
